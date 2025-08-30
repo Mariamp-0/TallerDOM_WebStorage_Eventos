3a815753753fbc109259660f730626c2249d9a74
Maria Paula Mejía <mapis4045@gmail.com>
2025-08-29 21:54:49 -0500

    Solution for workshop adding tasks to local storage using events and DOM

---
+++ script.js
@@ -0,0 +1,65 @@
+
+const taskInput = document.getElementById('task-input');  
+const btnAdd = document.getElementById('add-task-btn'); 
+const taskList = document.getElementById('task-list');
+
+function createTaskElement(taskText) {
+    let list = document.createElement("li")
+    list.className = "task-item"
+    list.textContent = taskText
+
+    let btnDelete = document.createElement("button")
+    btnDelete.className = "delete-btn"
+    btnDelete.textContent = "Eliminar"
+
+    btnDelete.addEventListener("click", () => {
+        list.remove()
+        saveTasks()
+
+    }) 
+
+    list.appendChild(btnDelete)
+    taskList.appendChild(list)
+
+    saveTasks()
+};
+
+
+btnAdd.addEventListener("click", () => {
+
+    const taskInputText = taskInput.value
+
+    if (taskInputText !== "") {
+        createTaskElement(taskInputText)
+        taskInput.value = ""
+    } else {
+        alert("No es posible agregar una tarea vacía")
+    }
+});
+
+
+function saveTasks() {
+
+    const itemsList = document.querySelectorAll(".task-item")
+    tasks = []
+
+    itemsList.forEach((item) => {
+        tasks.push(item.firstChild.textContent)
+    })
+
+    localStorage.setItem("tasks",JSON.stringify(tasks))
+
+};
+
+function loadTask() {
+
+    const savedTasks = JSON.parse(localStorage.getItem("tasks")) || [] 
+
+    if (savedTasks.length > 0 ) {
+        savedTasks.forEach((task) => {
+            createTaskElement(task)
+
+        })};
+};
+
+loadTask()
